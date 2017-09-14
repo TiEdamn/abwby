bd6c9091b7f9f8bf3e9dce1e03dab4566a3bf86f
TiEdamn <tikhanovich.89@gamil.com>
2017-09-15 01:14:47 +0300

    заготовка для яндекс карт

--- ajax/calc.php
+++ ajax/calc.php
@@ -1,21 +1,26 @@
 <?php
 
-$cat_diam = ceil(($_POST['cat-r'] * 2.54 + ($_POST['cat-1'] * $_POST['cat-2'] / 1000) * 2) * 100) / 100;
-$cat_width = ceil($_POST['cat-1'] / 10 * 100) / 100;
-$cat_height = ceil($_POST['cat-1'] * $_POST['cat-2'] / 1000 * 100) / 100;
-$new_diam = ceil(($_POST['new-r'] * 2.54 + ($_POST['new-1'] * $_POST['new-2'] / 1000) * 2) * 100) / 100;
-$new_width = ceil($_POST['new-1'] / 10 * 100) / 100;
-$new_height = ceil($_POST['new-1'] * $_POST['new-2'] / 1000 * 100) / 100;
-$real_speed = ceil(100 * $new_diam / $cat_diam) / 100;
+if($_POST)
+{
+    $cat_diam = ceil(($_POST['cat-r'] * 2.54 + ($_POST['cat-1'] * $_POST['cat-2'] / 1000) * 2) * 100) / 100;
+    $cat_width = ceil($_POST['cat-1'] / 10 * 100) / 100;
+    $cat_height = ceil($_POST['cat-1'] * $_POST['cat-2'] / 1000 * 100) / 100;
+    $new_diam = ceil(($_POST['new-r'] * 2.54 + ($_POST['new-1'] * $_POST['new-2'] / 1000) * 2) * 100) / 100;
+    $new_width = ceil($_POST['new-1'] / 10 * 100) / 100;
+    $new_height = ceil($_POST['new-1'] * $_POST['new-2'] / 1000 * 100) / 100;
+    $real_speed = ceil(100 * $new_diam / $cat_diam) / 100;
 
-$result = [
-    'cat_diam'      =>$cat_diam,
-    'cat_width'     =>$cat_width,
-    'cat_height'    =>$cat_height,
-    'new_diam'      =>$new_diam,
-    'new_width'     =>$new_width,
-    'new_height'    =>$new_height,
-    'real_speed'    =>$real_speed
-];
+    $result = [
+        'cat_diam'      =>$cat_diam,
+        'cat_width'     =>$cat_width,
+        'cat_height'    =>$cat_height,
+        'new_diam'      =>$new_diam,
+        'new_width'     =>$new_width,
+        'new_height'    =>$new_height,
+        'real_speed'    =>$real_speed
+    ];
 
-echo json_encode($result);
+    echo json_encode($result);
+} else {
+    header("Location: /");
+}
